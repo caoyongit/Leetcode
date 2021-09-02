da3a87cf602ed588e5453393ccb3064a8ed170b9
caoyong <caoyongit2007@gmail.com>
2021-09-02 17:58:20 +0800

    add others

--- src/com/BinarySearch.java
+++ src/com/BinarySearch.java
@@ -16,8 +16,8 @@ public class BinarySearch {
 
 
     public static void main(String[] args) {
-        int[] nums = new int[]{-1,0,3,5,9,12};
-        System.out.println(search(nums, 2));
+        int[] nums = new int[]{1,3,5,6};
+        System.out.println(searchInsert(nums, 0));
     }
 
 
@@ -40,4 +40,64 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    /**
+     * 704
+     */
+    public static int search1(int[] nums, int target) {
+        int len = nums.length;
+
+        int left = 0;
+        int right = len - 1;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if (nums[mid] > target) {
+                // 下一轮搜索区间是 [left, mid - 1]
+                right = mid - 1;
+            } else {
+                // 下一轮搜索区间是 [mid, right]
+                left = mid;
+            }
+        }
+
+        if (nums[left] == target) {
+            return left;
+        }
+        return -1;
+    }
+
+    /*
+     * 374 猜数字大小
+     */
+    /*public int guessNumber(int n) {
+        int left = 0, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (guess(mid) <= 0) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }*/
+
+    /**
+     * 35 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     * 请必须使用时间复杂度为 O(log n) 的算法。
+     */
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0,right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return right + 1;
+    }
+
 }
