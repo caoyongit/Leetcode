5d9c56df29d4532ef507159e59a017a38eede699
caoyong <caoyongit2007@gmail.com>
2021-09-05 14:15:16 +0800

    add others

--- src/com/BinarySearch.java
+++ src/com/BinarySearch.java
@@ -6,6 +6,8 @@
  */
 package com;
 
+import java.util.List;
+
 /**
  * 二分查找
  *
@@ -16,8 +18,8 @@ public class BinarySearch {
 
 
     public static void main(String[] args) {
-        int[] nums = new int[]{3,5,6,8,9};
-        System.out.println(searchFirstElement(nums, 8));
+        int[] nums = new int[]{2,2,2,0,1};
+        System.out.println(findMin(nums));
     }
 
 
@@ -148,6 +150,60 @@ public class BinarySearch {
             }
         }
         return left;
+    }
+
+
+    /**
+     * 153. 寻找旋转排序数组中的最小值
+     * 已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,2,4,5,6,7] 在变化后可能得到：
+     若旋转 4 次，则可以得到 [4,5,6,7,0,1,2]
+     若旋转 7 次，则可以得到 [0,1,2,4,5,6,7]
+     注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+
+     给你一个元素值 互不相同 的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
+     */
+    public static int findMin(int[] nums) {
+        int left = 0, len = nums.length;
+        int right = len - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
+    }
+
+    /**
+     * 154  已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,4,4,5,6,7] 在变化后可能得到：
+     若旋转 4 次，则可以得到 [4,5,6,7,0,1,4]
+     若旋转 7 次，则可以得到 [0,1,4,4,5,6,7]
+     注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+
+     给你一个可能存在 重复 元素值的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
+
+      
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public static int findMin2(int[] nums) {
+        int left = 0, len = nums.length;
+        int right = len - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else if (nums[mid] > nums[right]){
+                left = mid + 1;
+            } else {
+                right -= 1;
+            }
+        }
+        return nums[left];
     }
 
 }
