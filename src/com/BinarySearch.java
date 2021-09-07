c2f46f7cfff68abcb3498fac2df3d90e668be6d4
caoyong <caoyongit2007@gmail.com>
2021-09-07 15:23:05 +0800

    add others

--- src/com/BinarySearch.java
+++ src/com/BinarySearch.java
@@ -183,12 +183,6 @@ public class BinarySearch {
      注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
 
      给你一个可能存在 重复 元素值的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
-
-      
-
-     来源：力扣（LeetCode）
-     链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii
-     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
     public static int findMin2(int[] nums) {
         int left = 0, len = nums.length;
@@ -205,5 +199,43 @@ public class BinarySearch {
         }
         return nums[left];
     }
+
+    /**
+     * 整数数组 nums 按升序排列，数组中的值 互不相同 。
+     在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。
+     例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+     给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 
+     */
+    public static int searchSpin(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
 
 }
