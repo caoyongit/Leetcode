3f04e8c03f6ebcc903577dba9e34d2b011195887
caoyong <caoyongit2007@gmail.com>
2021-09-11 11:40:42 +0800

    add others

--- src/com/BinarySearch.java
+++ src/com/BinarySearch.java
@@ -251,5 +251,45 @@ public class BinarySearch {
         return -1;
     }
 
+    /**
+     * 278. 第一个错误的版本
+     * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+     * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+     * 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+     */
+    /*public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }*/
+
+    /**
+     * 69. x 的平方根
+     * 给你一个非负整数 x ，计算并返回 x 的 平方根 。
+     * 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+     * 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+     */
+    public static int mySqrt(int x) {
+        if (x == 0) {
+            return x;
+        }
+        int left = 1,right = x/2;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if (mid > x / mid) {
+                right = mid -1;
+            } else {
+                left = mid;
+            }
+        }
+        return left;
+    }
 
 }
--- src/com/Sorted.java
+++ src/com/Sorted.java
@@ -4,13 +4,14 @@ import java.util.Arrays;
 
 public class Sorted {
     public static void main(String[] args) {
-        int[] nums = new int[]{3,3};
-        Arrays.stream(twoSum(nums, 6)).forEach(s -> System.out.print(s + " "));
+        int[] nums = new int[]{5,2,3,2,1};
+        Arrays.stream(insertSorted2(nums)).forEach(s -> System.out.print(s + " "));
         //System.out.println(twoSum(nums, 9));
     }
 
     /**
      * 912 给你一个整数数组 nums，请你将该数组升序排列。
+     * 选择排序
      */
     public static int[] sortArray(int[] nums) {
         int len = nums.length;
@@ -49,6 +50,40 @@ public class Sorted {
             }
         }
         return new int[]{-1, -1};
+    }
+
+    /**
+     * 插入排序，逐个交换到前面合适的位置
+     */
+    public static int[] insertSorted(int []nums) {
+        int len = nums.length;
+        for (int i = 1; i < len; i++) {
+            for (int j = i; j > 0; j--) {
+                if (nums[j] <= nums[j - 1]) {
+                    swap(nums, j - 1, j);
+                } else {
+                    break;
+                }
+            }
+        }
+        return nums;
+    }
+
+    /**
+     * 插入排序2，先暂存当前变量，然后将前面的若干个元素逐个向后赋值
+     */
+    public static int[] insertSorted2(int []nums) {
+        int len = nums.length;
+        for (int i = 1; i < len; i++) {
+            int temp = nums[i];
+            int j = i;
+            while (j > 0 && nums[j-1] > temp) {
+                nums[j] = nums[j - 1];
+                j--;
+            }
+            nums[j] = temp;
+        }
+        return nums;
     }
 
 }
