db462274d50bbe61e44b7056eafa23871863565d
caoyong <caoyongit2007@gmail.com>
2021-10-14 23:41:28 +0800

    数组中的逆序对

--- src/com/MergeSort.java
+++ src/com/MergeSort.java
@@ -16,10 +16,10 @@ public class MergeSort {
 
     /**
      * 对数组 nums 的子区间 [left..right] 进行归并排序
-     * 
+     *
      * @param nums
      * @param left
-     * @param right 
+     * @param right
      * @author caoyong
      * @date 2021/10/9 10:31 上午
      */
@@ -35,10 +35,10 @@ public class MergeSort {
 
     /**
      * 合并两个有序数组：先把值复制到临时数组，再合并回去
-     * 
+     *
      * @param nums
      * @param left
-     * @param mid [left, mid] 有序，[mid + 1, right] 有序
+     * @param mid   [left, mid] 有序，[mid + 1, right] 有序
      * @param right
      * @author caoyong
      * @date 2021/10/9 10:32 上午
@@ -78,12 +78,76 @@ public class MergeSort {
         return nums;
     }
 
-    public static void main(String[] args) {
-        int []nums = new int[]{4,6,7,2,8,9,32,56};
-        int[]ary = sortArray(nums);
-        for (int a : ary) {
-            System.out.print(a + " ");
+
+    /**
+     * 剑指 Offer 51. 数组中的逆序对
+     * 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。
+     * 示例 1:
+     * 输入: [7,5,6,4]
+     * 输出: 5
+     * 限制：0 <= 数组长度 <= 50000
+     *
+     * @param nums
+     * @return
+     */
+    public static int reversePairs(int[] nums) {
+        int len = nums.length;
+        if (len < 2) {
+            return 0;
         }
-        System.out.println();
+
+        int[] copy = new int[len];
+        for (int i = 0; i < len; i++) {
+            copy[i] = nums[i];
+        }
+
+        int[] temp = new int[len];
+        return reversePairs(copy, 0, len - 1, temp);
+    }
+
+    private static int reversePairs(int[] nums, int left, int right, int[] temp) {
+        if (left == right)
+            return 0;
+
+        int mid = left + (right - left) / 2;
+        int leftPairs = reversePairs(nums, left, mid, temp);
+        int rightPairs = reversePairs(nums, mid + 1, right, temp);
+        if (nums[mid] <= nums[mid + 1]) {
+            return leftPairs + rightPairs;
+        }
+        int crossPairs = mergeAndCount(nums, left, mid, right, temp);
+        return  leftPairs + rightPairs + crossPairs;
+    }
+
+    private static int mergeAndCount(int[] nums, int left, int mid, int right, int[] temp) {
+        for (int i = left; i <= right; i++) {
+            temp[i] = nums[i];
+        }
+        int i = left;
+        int j = mid + 1;
+
+        int count = 0;
+        for (int k = left; k <= right; k++) {
+            if (i == mid + 1) {
+                nums[k] = temp[j];
+                j++;
+            } else if (j == right + 1) {
+                nums[k] = temp[i];
+                i++;
+            } else if (temp[i] <= temp[j]) {
+                nums[k] = temp[i];
+                i++;
+            } else {
+                nums[k] = temp[j];
+                j++;
+                count += (mid - i + 1);
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{7,5,6,4};
+        System.out.println(reversePairs(nums));
     }
 }
